6cb5785246be28e501edf648cd9779f6fc9ebcd8
ly1994lyy <1141730852@qq.com>
2024-05-06 12:28:24 +0800

    feat:参数校验&&异常返回报文封装

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -3,10 +3,14 @@ import { AdminModule } from './admin.module';
 import * as process from 'process';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { TransformInterceptor } from '@app/common/transform/transform.interceptor';
+import { ValidatePipe } from '@app/common/validate/validate.pipe';
+import { HttpExceptionFilter } from '@app/common/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidatePipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
   const options = new DocumentBuilder()
     .setTitle('基于Nestjs的一个通用后台管理系统接口api')
---
+++ libs/common/src/validate/validate.pipe.ts
@@ -0,0 +1,34 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class ValidatePipe implements PipeTransform {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToInstance(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      throw new BadRequestException({
+        message: Object.values(errors[0].constraints)[0],
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+    console.log(errors);
+
+    return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
